c2919b54a5f645732b042ded61d2bfdcdf29de74
GAYATHRI-2002 <83536175+GAYATHRI-2002@users.noreply.github.com>
2022-02-25 21:59:45 +0530

    Create file1.java

---
+++ cycle5/file1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class file1
+{
+   public ststic void main(String args[])
+   {
+     Fileinputstream fin=new Fileinputstream("f1.txt");
+     int i=1;
+     int ch;
+     System.out.println(i+"-");
+     while((ch=file.read()!=-1)
+            {
+             System.out.println(++i+"-");
+            }
+           }
+               file.close();
+           }
+        }
